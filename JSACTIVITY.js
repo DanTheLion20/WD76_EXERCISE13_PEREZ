565c32be9c959ab6cada3587b8963fe162557cff
DanTheLion20 <danphilip.rsh@gmail.com>
2023-07-28 15:00:27 +0800

    FINAL OUTPUT

--- JSACTIVITY.js
+++ JSACTIVITY.js
@@ -1,20 +1,35 @@
-function ounce() {
-
+function convertkg(unitConverter) {
     var data = document.getElementsByName('massConverter')[0].value;
-    var result = parseFloat(data) * 35.274;
+    var result = parseFloat(data) * unitConverter;
     document.getElementsByName('output')[0].value = result;
+}
+
+function ounce() {
+    convertkg(35.274);
 }
 
 function pound() {
-
-    var data = document.getElementsByName('massConverter')[0].value;
-    var result = parseFloat(data) * 2.205;
-    document.getElementsByName('output')[0].value = result;
+    convertkg(2.205);
 }
 
 function gram() {
+    convertkg(1000);
+}
 
+function convertstone(unitConverter) {
     var data = document.getElementsByName('massConverter')[0].value;
-    var result = parseFloat(data) * 1000;
+    var result = parseFloat(data) * unitConverter;
     document.getElementsByName('output')[0].value = result;
+}
+
+function ounce() {
+    convertstone(224);
+}
+
+function pound() {
+    convertstone(14);
+}
+
+function gram() {
+    convertstone(6350);
 }
